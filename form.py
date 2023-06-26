25453e7956809953a2002da1c39ace376211c480
surajchauhan56 <137756285+surajchauhan56@users.noreply.github.com>
2023-06-26 12:25:37 +0530

    Add files via upload

---
+++ form.py
@@ -0,0 +1,14 @@
+import streamlit as st
+with st.form('form'):
+    st.write('Please fill the form')
+    name=st.text_input('Name')
+    college_name=st.text_input('Your college name')
+    Percentage=st.number_input('Your Percentage')
+    Working=st.checkbox('Are you working')
+
+    submitted=st.form_submit_button('Submit')
+    if submitted:
+        st.write('name:',name)
+        st.write('college_name:',college_name)
+        st.write('Percentage:',Percentage)
+        st.write('Are you working',Working)
